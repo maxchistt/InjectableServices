76861ff0bdb00044fcf8076a7f1cd98e825f6c11
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-11-16 13:43:39 +0600

    Добавьте файлы проекта.

---
+++ InjectableServices/InjectableAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace InjectableServices
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class InjectableAttribute : Attribute
+    {
+        public ServiceLifetime Lifetime { get; }
+
+        public InjectableAttribute(ServiceLifetime Lifetime = ServiceLifetime.Scoped)
+        {
+            this.Lifetime = Lifetime;
+        }
+    }
+}
---
+++ InjectableServices/InjectableServicesExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace InjectableServices
+{
+    public static class InjectableServicesExtension
+    {
+        public static void AddInjectableServices(this IServiceCollection services)
+        {
+            var classesWithAttribute = Assembly.GetEntryAssembly()!.GetTypes()
+                .Where(type => type.IsClass && type.GetCustomAttribute<InjectableAttribute>() is not null);
+
+            foreach (var classType in classesWithAttribute)
+            {
+                if (classType is null) continue;
+                var attr = classType.GetCustomAttribute<InjectableAttribute>();
+                if (attr is null) continue;
+
+                Debug.WriteLine($"{classType.Name} is {attr.Lifetime} service");
+
+                foreach (var interfaceType in classType.GetInterfaces())
+                {
+                    services.Add(new(interfaceType, classType, attr.Lifetime));
+                    Debug.WriteLine($"{classType.Name} added as {attr.Lifetime} service of type {interfaceType.Name}");
+                }
+            }
+        }
+    }
+}
